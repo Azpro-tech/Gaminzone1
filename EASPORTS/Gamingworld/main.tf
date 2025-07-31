738d76e60fd037338e7ba57f4be50c9f3634d0d2
unknown <azimraja85@gmail.com>
2025-07-31 18:12:18 +0530

    feature002

--- EASPORTS/Gamingworld/main.tf
+++ EASPORTS/Gamingworld/main.tf
@@ -6,3 +6,20 @@ module "vnet1" {
   address_space = ["10.0.0.0/16"]
 }
 
+module "subnet1" {
+  source           = "../../modules/subnet"
+  depends_on       = [module.vnet1]
+  subnet_name      = "frontend-subnet"
+  vnet_name        = "vnet999"
+  rg_name          = "azim-rg"
+  address_prefixes = ["10.0.1.0/24"]
+}
+
+module "subnet2" {
+  source           = "../../modules/subnet"
+  depends_on       = [module.vnet1]
+  subnet_name      = "backend-subnet"
+  vnet_name        = "vnet999"
+  rg_name          = "azim-rg"
+  address_prefixes = ["10.0.2.0/24"]
+}
